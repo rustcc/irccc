49acf4eae3fded58ec1fe542f215b8ff35036d45
Daogang Tang <daogangtang@126.com>
2014-12-30 00:24:32 +0800

    format main.rs.
    Signed-off-by: Daogang Tang <daogangtang@126.com>

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,22 @@ use websocket::header::WebSocketProtocol;
 //use openssl::x509::X509FileType;
 
 fn main() {
-	/*
-	let mut context = SslContext::new(SslMethod::Tlsv1).unwrap();
-	let _ = context.set_certificate_file(&(Path::new("cert.pem")), X509FileType::PEM);
-	let _ = context.set_private_key_file(&(Path::new("key.pem")), X509FileType::PEM);
-	
-	let server = WebSocketServer::bind_secure("127.0.0.1:2794", &context).unwrap();
-	*/
-	let server = WebSocketServer::bind("127.0.0.1:8080").unwrap();
-	
-	let mut acceptor = server.listen().unwrap();
+    /*
+       let mut context = SslContext::new(SslMethod::Tlsv1).unwrap();
+       let _ = context.set_certificate_file(&(Path::new("cert.pem")), X509FileType::PEM);
+       let _ = context.set_private_key_file(&(Path::new("key.pem")), X509FileType::PEM);
+
+       let server = WebSocketServer::bind_secure("127.0.0.1:2794", &context).unwrap();
+       */
+    let server = WebSocketServer::bind("127.0.0.1:8080").unwrap();
+
+    let mut acceptor = server.listen().unwrap();
 
     let mut all_clients = Vec::new();
     let (tx, rx) = comm::channel();
     let (ctx, crx) = comm::channel();
-	let mut id = 0u;
-    
+    let mut id = 0u;
+
     Thread::spawn(move || {
         for request in acceptor.incoming() {
             id += 1;
@@ -110,10 +110,10 @@ fn main() {
             _ => {}
         }
         for client in all_clients.iter_mut() {
-           let message = WebSocketMessage::Text("haha, im".to_string());
-           //let mut mclient = client.clone();
-           client.send_message(message);
-            
+            let message = WebSocketMessage::Text("haha, im".to_string());
+            //let mut mclient = client.clone();
+            client.send_message(message);
+
         }
     }
 }
